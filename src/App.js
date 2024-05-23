69679af89a66e5fdf61b0fb030ceb8e3f26e87ac
Lucas Camarino Emerick de Almeida <104381447+lunatos@users.noreply.github.com>
2024-05-23 15:40:30 -0300

    start
    navBar-creating

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
         <h1>Hello World</h1>
-      </header>
-    </div>
+      </Router>
+    </>
   );
 }
 
