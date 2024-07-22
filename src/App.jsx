3ab16af7b75e5528a2db2d9d71e7493d2f75c1fb
Lucas Camarino Emerick de Almeida <lucascamarino@gmail.com>
2024-07-22 17:22:21 -0300

    Nowww is fixed
    Nowww is fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from './components/nav-bar/NavBar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/home';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,10 +10,10 @@ import Projects from './components/pages/projects';
 function App() {
   return (
     <>
-      <Router basename='/home/'>
+      <Router>
         <NavBar />
         <Routes>
-          <Route path='/' exact element={<Home />} />
+          <Route path='/home' exact element={<Home />} />
           <Route path='/bio' exact element={<Bio />} />
           <Route path='/projects' exact element={<Projects />} />
         </Routes>
